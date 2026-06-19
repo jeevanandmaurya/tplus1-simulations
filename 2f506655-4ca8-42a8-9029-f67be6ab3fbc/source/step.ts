695235c77d35d452743e245b364d3073fe701907
Jeevanand Maurya <jeevanandmaurya@gmail.com>
2026-06-20 04:59:49 +0530

    Delete simulation

--- 2f506655-4ca8-42a8-9029-f67be6ab3fbc/source/step.ts
+++
@@ -1,6 +0,0 @@
-// Ball Drop Logic
-export function step(state: any, config: any, dt: number) {
-  // Rapier3D handles the physics automatically based on rigid bodies!
-  // We don't need to manually update positions here.
-  return state;
-}
